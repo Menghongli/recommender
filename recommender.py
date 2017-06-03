e96e8e0b2989808d3e10cf2c0ad97559604c7869
Menghong Li <menghong@redant.com.au>
2017-06-03 17:23:51 +1000

    SVD

--- recommender.py
+++ recommender.py
@@ -1,7 +1,10 @@
+from scipy import linalg as la
+
 import numpy as np
 import pickle as pk
 import tool
 import similarity
+import gzip
 
 class Recommender:
     def __init__(self, dataMat, simMeasure):
@@ -66,3 +69,68 @@ class UserBased(Recommender):
             ratSimTotal += similarity * userRating
         if simTotal == 0: return 0
         else: return ratSimTotal/simTotal
+
+
+class MatrixFactorization(Recommender):
+    def __init__(self, K):
+        Recommender.__init__(self, dataMat=None, simMeasure=None)
+        self.K = K
+        self.users, self.books, self.dataMat = tool.loadRating()
+        self.X, self.Q = self.train()
+
+
+    def train(self, steps=5000, alpha=0.0002, beta=0.02):
+        n_users = len(self.dataMat)
+        n_books = len(self.dataMat[0])
+
+        U,Sigma, VT = la.svds(self.dataMat, return_singular_vectors='u')
+        # Sig2 = Sigma**2
+        # totalEng = sum(Sig2) * 0.9
+        # dim = 0
+
+        # for u in range(n_users):
+        #     if sum(Sig2[:u]) >= totalEng:
+        #         dim = u
+        #         break
+        SigDim = np.mat(np.eye(6)*Sigma[:6])
+
+        transformedUser = (self.dataMat.T * U[:, :6] * SigDim.I).T
+
+        X = np.random.rand(n_users, self.K)
+        Q = np.random.rand(n_books, self.K)
+        Q = Q.T
+
+        for step in range(steps):
+            print("Step: {}".format(step))
+            for i in range(dim):
+                print('.', end='', flush=True)
+                for j in range(n_books):
+                    if self.dataMat[i][j] > 0:
+                        er = np.dot(X[i,:], Q[:, j]) - self.dataMat[i][j]
+                        for k in range(self.K):
+                            temp_x = X[i][k] - alpha * (er * Q[k][j] + beta * X[i][k])
+                            temp_q = Q[k][j] - alpha * (er * X[i][k] + beta * Q[k][j])
+
+                            X[i][k] = temp_x
+                            Q[k][j] = temp_q
+
+            cost = 0
+            for i in range(n_users):
+                for j in range(n_books):
+                    if self.dataMat[i][j] > 0:
+                        cost = cost + (self.dataMat[i][j] - np.dot(X[i,:],Q[:,j]))**2
+                        for k in range(self.K):
+                            cost = cost + beta * (X[i][k]**2 + Q[k][j]**2)
+
+                        cost = cost/2
+            if cost < 0.1:
+                break
+
+        return X, Q.T
+
+    def predict(self, user, book):
+        R = np.dot(self.X, self.Q.T)
+        u_index = self.users[user]
+        b_index = self.books[book]
+
+        return R[u_index][b_index]
--- tool.py
+++ tool.py
@@ -2,10 +2,13 @@ import csv
 import pickle as pk
 import gzip
 import random
+import numpy as np
 
 def dumpDataMat():
     userMat = {}
     bookMat = {}
+    books   = {}
+    users   = {}
 
     with open('data/BX-Book-Ratings.csv', 'rt', encoding="iso-8859-1") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
@@ -17,11 +20,58 @@ def dumpDataMat():
             bookMat.setdefault(row["ISBN"], {})
             bookMat[row["ISBN"]][row["User-ID"]] = int(row["Book-Rating"])
 
+    with open('data/BX-Books.csv', 'rt', encoding="iso-8859-1") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
+        index = 0
+        for row in reader:
+            if row["ISBN"] in bookMat:
+                books[row["ISBN"]] = index
+                index += 1
+
+    with open('data/BX-Users.csv', 'rt', encoding="iso-8859-1") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
+        index = 0
+        for row in reader:
+            if row["User-ID"] in userMat:
+                users[row["User-ID"]] = index
+                index += 1
+
+    with gzip.open('Users.pklz', 'wb') as output:
+        pk.dump(users, output)
+
+    with gzip.open('Books.pklz', 'wb') as output:
+        pk.dump(books, output)
+
     with gzip.open('UserMat.pklz', 'wb') as output:
         pk.dump(userMat, output)
 
     with gzip.open('BookMat.pklz', 'wb') as output:
         pk.dump(bookMat, output)
+
+def loadRating():
+    users = None
+    books = None
+    userMat = loadUserMat()
+
+    with gzip.open('Users.pklz', 'rb') as input:
+        users = pk.load(input)
+
+    with gzip.open('Books.pklz', 'rb') as input:
+        books = pk.load(input)
+
+    n_users = len(users.keys())
+    n_books = len(books.keys())
+
+    ratingsMat = np.zeros(shape=(n_users, n_books), dtype=np.float64)
+
+    for user, ratings in userMat.items():
+        for book, rating in ratings.items():
+            if user in users.keys() and book in books.keys():
+                u_index = users[user]
+                b_index = books[book]
+                ratingsMat[u_index][b_index] = rating
+
+    return users, books, ratingsMat
 
 def loadUserMat():
     dataMat = None
